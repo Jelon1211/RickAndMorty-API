7f8766610e3cecd30c63d5b5d947987701e65b36
Jelon <jelon1211@gmail.com>
2022-06-26 10:26:45 +0200

    d

--- src/RicknMorty.tsx
+++ src/RicknMorty.tsx
@@ -21,7 +21,7 @@ const RicknMorty = () => {
   const [location, setLocation] = useState<string>("");
   const [idChar, setIdChar] = useState<string>("0");
   const [isActive, setIsActive] = useState<boolean>(false);
-  // comments are one c
+  // comments are one d
   const fetchBtn = () => {
     axios
       .get(`https://rickandmortyapi.com/api/character/${idChar}`)
